61b3f00c4ef47044c0d66cd716d237ab8482426c
LeeSM0518 <nalsm98@naver.com>
2024-08-07 23:25:18 +0900

    dep: Serializable 플러그인 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
+    kotlin("plugin.serialization") version "1.8.0"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
     id("com.google.osdetector") version "1.7.0"
