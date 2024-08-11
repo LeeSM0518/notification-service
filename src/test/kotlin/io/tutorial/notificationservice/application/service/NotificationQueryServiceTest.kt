2e450e6138c2f4e9e2bd6b0adb6ea264aff76d32
LeeSM0518 <nalsm98@naver.com>
2024-08-11 23:28:11 +0900

    test: 알림 페이징 조회 테스트

--- src/test/kotlin/io/tutorial/notificationservice/application/service/NotificationQueryServiceTest.kt
+++ src/test/kotlin/io/tutorial/notificationservice/application/service/NotificationQueryServiceTest.kt
@@ -1,5 +1,6 @@
 package io.tutorial.notificationservice.application.service
 
+import io.tutorial.notificationservice.adapter.`in`.dto.GetNotificationsRequest
 import io.tutorial.notificationservice.adapter.out.persistence.NotificationEntity
 import io.tutorial.notificationservice.adapter.out.persistence.NotificationRepository
 import io.tutorial.notificationservice.config.IntegrationTest
@@ -22,7 +23,7 @@ internal class NotificationQueryServiceTest @Autowired constructor(
 
     @BeforeTest
     fun setup() = runTest {
-        expected = NotificationEntity(
+        val entity = NotificationEntity(
             type = NotificationType.REVIEW,
             content = "content",
             note = "note",
@@ -30,7 +31,7 @@ internal class NotificationQueryServiceTest @Autowired constructor(
             notifiedDate = Instant.now(),
             checked = false
         )
-        notificationRepository.save(expected)
+        expected = notificationRepository.save(entity)
     }
 
     @AfterTest
@@ -48,5 +49,27 @@ internal class NotificationQueryServiceTest @Autowired constructor(
 
         // then
         assertThat(count).isEqualTo(1)
+    }
+
+    @Test
+    fun `알림을 페이지로 조회할 수 있다`() = runTest {
+        // given
+        val request = GetNotificationsRequest(expected.receiverId, 0, 10)
+
+        // when
+        val notifications = notificationQueryService.getNotifications(request)
+
+        // then
+        val notificationResponses = notifications.content
+        val response = notificationResponses[0]
+        assertThat(response.id).isEqualTo(expected.id)
+        assertThat(response.note).isEqualTo(expected.note)
+        assertThat(response.type).isEqualTo(expected.type)
+        assertThat(response.content).isEqualTo(expected.content)
+        assertThat(response.checked).isEqualTo(expected.checked)
+        assertThat(response.receiverId).isEqualTo(expected.receiverId)
+        assertThat(response.notifiedDate).isEqualTo(expected.notifiedDate)
+        assertThat(notifications.totalPages).isOne()
+        assertThat(notifications.totalElements).isOne()
     }
 }
